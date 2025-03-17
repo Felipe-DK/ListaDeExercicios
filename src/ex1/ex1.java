42b23e9ecfa3df2b9a6544c44974e6de0b30d8f3
Felipe-DK <felipesocialnw@gmail.com>
2025-03-17 12:31:09 -0300

    Adicionando exercicio 1 removido App.java

---
+++ src/ex1/ex1.java
@@ -0,0 +1,26 @@
+package ex1;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // leitura de variaveis
+        System.out.println("Insira o valor A");
+        Long valorA = scanner.nextLong();
+        System.out.println("Insira o valor B");
+        Long valorB = scanner.nextLong();
+        System.out.println("Insira o valor C");
+        Long valorC = scanner.nextLong();
+
+        // resultado da soma
+        Long soma = valorA + valorB;
+        System.out.println("A soma dos valores " + valorA + " e " + valorB + " é: " + soma);
+
+        // operador ternario que faz a varificacao
+        System.out.println((soma < valorC) ? "Resulado menor que C" : "Resultado maior que C");
+
+        scanner.close();
+    }
+}
