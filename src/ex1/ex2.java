11688e8ac6a3ca6ce20724f7d77c34954204e216
Felipe-DK <felipesocialnw@gmail.com>
2025-03-17 13:11:33 -0300

    Adicionando o exercicio 2

---
+++ src/ex1/ex2.java
@@ -0,0 +1,29 @@
+package ex1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = 0;
+        boolean entradaValida = false;
+
+        while (!entradaValida) {
+            try {
+                System.out.println("Digite um numero inteiro: ");
+                numero = scanner.nextInt();
+                entradaValida = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida! Por favor, digite um número inteiro.");
+                scanner.next();
+            }
+        }
+
+        System.out.println((numero % 2 == 0 ? numero + " é PAR" : numero + " é IMPAR"));
+
+
+        scanner.close();
+    }
+}
